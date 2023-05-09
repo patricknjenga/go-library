88e57fc2a33ac6a1ac2540acde49706a4b496040
patrick <patrick.njenga@outlook.com>
2023-05-10 00:07:10 +0300

    ++

--- postgres.go
+++ postgres.go
@@ -8,18 +8,13 @@ import (
 
 type Postgres struct {
 	*gorm.DB
-	Host     string
-	Models   []interface{}
-	Name     string
-	Password string
-	Port     string
-	Timezone string
-	User     string
+	Models []interface{}
+	Name   string
 }
 
-func (p Postgres) New() (Postgres, error) {
+func (p Postgres) New(r Redis) (Postgres, error) {
 	var err error
-	p.DB, err = gorm.Open(postgres.Open(fmt.Sprintf("dbname=%s host=%s user=%s password=%s port=%s sslmode=disable TimeZone=%s", p.Name, p.Host, p.User, p.Password, p.Port, p.Timezone)), &gorm.Config{})
+	p.DB, err = gorm.Open(postgres.Open(fmt.Sprintf("dbname=%s host=%s user=%s password=%s port=%s sslmode=disable TimeZone=%s", p.Name, r.GetSecret("DB_HOST"), r.GetSecret("DB_USER"), r.GetSecret("DB_PASS"), r.GetSecret("DB_PORT"), "Africa/Nairobi")), &gorm.Config{})
 	if err != nil {
 		return p, err
 	}
