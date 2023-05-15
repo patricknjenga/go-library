dfe4c9ac2fb6a054f68f6a15db1e8f2b13df5404
patrick <patrick.njenga@outlook.com>
2023-05-15 03:38:26 +0300

    ++

--- smpp.go
+++ smpp.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 )
 
-type Sms struct {
-	Message string
-	Phone   string
+type Smpp struct {
+	Messages   []string
+	Recipients []string
 }
 
-func (s Sms) Send(r Redis) ([]byte, error) {
+func (s Smpp) Send(r Redis) error {
 	ssh, err := Ssh{Address: r.GetSecret("SMPP_SSH_HOST"), Password: r.GetSecret("SMPP_SSH_PASS"), Port: r.GetSecret("SMPP_SSH_PORT"), User: r.GetSecret("SMPP_SSH_USER")}.New(r)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return ssh.Exec(fmt.Sprintf(`perl <<<"
-		use Net::SMPP;
-		Net::SMPP
-		->new_transceiver('%s', port => '%s', system_id => '%s', password => '%s', system_type => 'smpp')
-		->submit_sm(destination_addr => '%s', short_message => '%s');
-	"`, r.GetSecret("SMPP_HOST"), r.GetSecret("SMPP_PORT"), r.GetSecret("SMPP_USER"), r.GetSecret("SMPP_PASS"), s.Phone, s.Message))
+	for _, v := range s.Recipients {
+		for _, w := range s.Messages {
+			_, err := ssh.Exec(fmt.Sprintf(`perl <<<"
+				use Net::SMPP;
+				Net::SMPP
+				->new_transceiver('%s', port => '%s', system_id => '%s', password => '%s', system_type => 'smpp')
+				->submit_sm(destination_addr => '%s', short_message => '%s');
+			"`, r.GetSecret("SMPP_HOST"), r.GetSecret("SMPP_PORT"), r.GetSecret("SMPP_USER"), r.GetSecret("SMPP_PASS"), v, w))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
